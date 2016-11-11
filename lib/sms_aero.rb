0e9ecc2c34a348674d4fcf500f21bbc6a6f3d68b
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-11-11 14:52:09 +0300

    Add default settings

--- lib/sms_aero.rb
+++ lib/sms_aero.rb
@@ -11,4 +11,36 @@ class SmsAero
   %w(types operations scopes).each do |folder|
     Dir["sms_aero/#{folder}"].each { |file| require_relative file }
   end
+
+  settings do
+    using type: Types::Strict::String do
+      option :user
+      option :password
+    end
+
+    using type: Types::Form::Bool, default: -> { true } do
+      option :use_ssl,  default: -> { true }
+      option :use_post, default: -> { true }
+    end
+  end
+
+  base_url do |settings|
+    "http#{"s" if settings.use_ssl}://gate.smsaero.ru/"
+  end
+
+  operation do |settings|
+    documentation "https://smsaero.ru/api/description/"
+
+    http_method(settings.use_post ? :post : :get)
+
+    security do |user: nil, password: nil, **|
+      key_auth :user,     user     || settings.user,     using: :query
+      key_auth :password, password || settings.password, using: :query
+      key_auth :answer,   "json",                        using: :query
+    end
+
+    operation 200, 201 do |body:, **|
+      Hashie::Mash.new JSON.parse(body)
+    end
+  end
 end
