d3662c4323962bfb6eed570d0042290955f9bc41
Andrew Kozin (aka nepalez) <andrew.kozin@gmail.com>
2016-11-18 17:13:18 +0300

    Implement :check_sign operation

---
+++ spec/sms_aero/operations/check_sign_spec.rb
@@ -0,0 +1,88 @@
+RSpec.describe "operations[:check_sign]", "#call" do
+  let(:settings)  { { user: "LOGIN", password: "PASSWORD" } }
+  let(:client)    { SmsAero.new settings }
+  let(:operation) { client.operations[:check_sign] }
+  let(:params)    { { sign: "foo" } }
+  let(:answer)    { %w(accepted pending) }
+
+  before  { stub_request(:any, //).to_return(body: answer.to_json) }
+  subject { operation.call(params) }
+
+  context "using ssl via POST:" do
+    let(:host)  { "https://gate.smsaero.ru/sign" }
+    let(:query) { "answer=json&password=PASSWORD&sign=foo&user=LOGIN" }
+
+    it "sends a request" do
+      subject
+      expect(a_request(:post, "#{host}?#{query}")).to have_been_made
+    end
+
+    it "returns statuses" do
+      expect(subject.statuses).to eq %w(accepted pending)
+    end
+  end
+
+  context "via GET:" do
+    let(:host)  { "https://gate.smsaero.ru/sign" }
+    let(:query) { "answer=json&password=PASSWORD&sign=foo&user=LOGIN" }
+
+    before { settings[:use_post] = false }
+
+    it "sends a request" do
+      subject
+      expect(a_request(:get, "#{host}?#{query}")).to have_been_made
+    end
+  end
+
+  context "not using ssl:" do
+    let(:host)  { "http://gate.smsaero.ru/sign" }
+    let(:query) { "answer=json&password=PASSWORD&sign=foo&user=LOGIN" }
+
+    before { settings[:use_ssl] = false }
+
+    it "sends a request" do
+      subject
+      expect(a_request(:post, "#{host}?#{query}")).to have_been_made
+    end
+  end
+
+  context "with custom user:" do
+    let(:host)  { "https://gate.smsaero.ru/sign" }
+    let(:query) { "answer=json&password=PASSWORD&sign=foo&user=USER" }
+
+    before { params[:user] = "USER" }
+
+    it "sends a request" do
+      subject
+      expect(a_request(:post, "#{host}?#{query}")).to have_been_made
+    end
+  end
+
+  context "with custom password:" do
+    let(:host)  { "https://gate.smsaero.ru/sign" }
+    let(:query) { "answer=json&password=PSWD&sign=foo&user=LOGIN" }
+
+    before { params[:password] = "PSWD" }
+
+    it "sends a request" do
+      subject
+      expect(a_request(:post, "#{host}?#{query}")).to have_been_made
+    end
+  end
+
+  context "with invalid sign:" do
+    before { params[:sign] = "" }
+
+    it "raises an exception" do
+      expect { subject }.to raise_error(TypeError)
+    end
+  end
+
+  context "without a sign:" do
+    before { params.delete :sign }
+
+    it "raises an exception" do
+      expect { subject }.to raise_error(KeyError)
+    end
+  end
+end
